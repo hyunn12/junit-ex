101a6c78c896c547b0a776d9de4c919b9d2261cc
hyunn12 <hyunn964@gmail.com>
2023-01-07 20:42:07 +0900

    Chapter1 - JUnit을 이용한 테스트생성 - Hamcrest 대신 AssertJ 사용해보기

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # 자바와 JUnit을 활용한 실용주의 단위 테스트 연습코드
+
+### 사용 기술
+- Java 17
+- JUnit5
+
+
+## 1. JUnit 테스트 만들기
+[ScoreCollectionTest](./src/test/java/com/example/junitex/ScoreCollectionTest.java)
+
+### `Assertions.fail()`
+강제로 테스트를 실패시킴
+
+#### 사용하는 이유?
+
+참고: https://www.baeldung.com/junit-fail
+
+ 1. 아직 테스트코드가 덜짜여진 경우
+ 2. 테스트가 실패할 것 같을 때 예외가 발생할 것 같은 부분에서 바로 실패시키려는 경우
+ 3. 결과값이 원하는 값이 아닌 경우
+
+----
+
+### Hamcrest vs AssertJ
+- Hamcrest : `assertThat(T actual, Matcher<? super T> matcher)`
+- AssertJ : `assertThat(T actual)`
+
+#### AssertJ를 더 권장하는 이유?
+- Hamcrest 는 사용할 Matcher 를 외워서 맞춰서 사용해야함 
+- AssertJ 는 체이닝 방식을 사용해서 `asserThat().isEqualTo()` 처럼 사용해 좀 더 작관적으로 표현가능
+- SpringBoot 사용 시 `spring-boot-starter-test` 를 주로 사용하는데 여기에 AssertJ가 포함되어있어 따로 라이브러리를 포함할 필요가 없음
+
--- src/test/java/com/example/junitex/ScoreCollectionTest.java
+++ src/test/java/com/example/junitex/ScoreCollectionTest.java
@@ -8,24 +8,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreCollectionTest {
 
+    // JUnit5 부터는 메소드의 앞에 접근제한자를 꼭 명시하지 않아도됨
+
+    // 테스트 메소드를 명명할 땐 정확히 어떤 테스트인지 명시하는 것이 중요함
+    // 하지만 영어로 지으면 바로 내용을 이해하기 어렵기 때문에 아래처럼 @DisplayName 을 이용해 한 눈에 어떤 테스트인지 알아보기 쉽게 할 수 있음
+
     @Test
     @DisplayName("fail 을 이용한 의도적인 테스트 실패")
     void testFail() {
         fail("Not yet implemented");
+        // fail을 사용하면 무조건 테스트가 실패하는데 어떨 때 쓰는건데?
+        // 참고: https://www.baeldung.com/junit-fail
+        // 1. 아직 테스트코드가 덜짜여진 경우
+        // 2. 테스트가 실패할 것 같을 때 예외가 발생할 것 같은 부분에서 바로 실패시키려는 경우
+        // 3. 결과값이 원하는 값이 아닌 경우
     }
 
     @Test
     @DisplayName("arithmeticMean 2개 숫자메소드 테스트")
     void testArithmeticMean() {
-        // 준비
+        // 준비 Arrange
         ScoreCollection collection = new ScoreCollection();
         collection.add(() -> 5);
         collection.add(() -> 7);
 
-        // 실행
+        // 실행 Act
         int actualResult = collection.arithmeticMean();
 
-        // 단언
+        // 단언 Assert
         assertThat(actualResult).isEqualTo(6);
         // 책에서는 Hamcrest.CoreMatchers.assertThat 을 사용했지만 나는 주로 Springboot를 사용하고 그에 포함된 spring-boot-starter-test 를 사용할 것이기 때문에 거기에 포함된 AssertJ를 사용해봄
         // AssertJ vs Hamcrest
